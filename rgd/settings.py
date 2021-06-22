c93da6ac3d697357e2288ca8fdad08b21e11b98e
Abhishek Venkat <abhisaivenkat@gmail.com>
2021-06-22 13:43:08 -0400

    add socat service

--- rgd/settings.py
+++ rgd/settings.py
@@ -38,7 +38,7 @@ class GeoDjangoMixin(ConfigMixin):
             # TODO: Log that we aren't using the expected GDAL wheel?
             pass
     
-        configuration.ALLOWED_HOSTS = ['18.217.111.240']
+        configuration.ALLOWED_HOSTS = ['3.15.25.207']
 
 
 class SwaggerMixin(ConfigMixin):
